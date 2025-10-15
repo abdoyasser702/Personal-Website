c4032e81614820bf2a130b44877eb9312db0f680
Abdelrahman Yasser <abdelrahmanyasser702@gmail.com>
2025-10-15 10:29:20 +0300

    Update BioSection.tsx

--- src/components/BioSection.tsx
+++ src/components/BioSection.tsx
@@ -90,7 +90,7 @@ const BioSection = () => {
             Say Hi
           </a>
           <a
-            href="My CV.pdf"
+            href="/My CV.pdf"
             className={`${styles.bio__links} ${styles.find_out_link}`}
           >
             Download CV<span className={styles.bio__arrow}>↓</span>
